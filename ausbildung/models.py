a48e7c07bafcd37c27af9b401bfa80fe29b46ce0
Stefan Reinhard <sr@feinheit.ch>
2012-12-02 23:41:37 +0100

    finish section navigation bullshit for now

--- ausbildung/models.py
+++ ausbildung/models.py
@@ -7,6 +7,10 @@ from feincms.content.richtext.models import RichTextContent
 from feincms.content.section.models import SectionContent
 from feincms.content.medialibrary.v2 import MediaFileContent
 
+from feincms_cleanse import cleanse_html
+
+from BeautifulSoup import BeautifulSoup
+
 Page.register_extensions()
 
 Page.register_templates({
@@ -26,8 +30,25 @@ Page.register_templates({
 
 Page.register_extensions('titles')
 
-Page.create_content_type(RichTextContent)
-Page.create_content_type(SectionContent, TYPE_CHOICES=(
+# Patch Section content to support cleanse and save anchors on headings
+def save(self, *args, **kwargs):
+    if getattr(self, 'cleanse', False):
+        try:
+            self.richtext = self.cleanse(self.richtext)
+        except TypeError:
+            self.richtext = self.cleanse.im_func(self.richtext)
+
+    soup = BeautifulSoup(self.richtext)
+    for i, h3 in enumerate(soup.findAll('h3')):
+        h3['id'] = u'section%d.%d' % (self.id, i+1)
+
+    self.richtext = soup.prettify()
+
+    super(SectionContent, self).save(*args, **kwargs)
+SectionContent.save = save
+
+Page.create_content_type(RichTextContent, cleanse=cleanse_html)
+Page.create_content_type(SectionContent, cleanse=cleanse_html, TYPE_CHOICES=(
      ('menu', 'Mit Menü'),
 ))
 Page.create_content_type(MediaFileContent, TYPE_CHOICES=(
---
+++ ausbildung/templatetags/sectionmenu.py
@@ -0,0 +1,38 @@
+from django import template
+
+from BeautifulSoup import BeautifulSoup
+
+register = template.Library()
+
+
+@register.inclusion_tag('sectionmenu.html')
+def sectionmenu(feincms_page):
+    menu = []
+
+    for section in feincms_page.sectioncontent_set.all():
+        entry = {
+            'title': section.title,
+            'anchor': u'#section%d' % section.id
+        }
+
+        soup = BeautifulSoup(section.richtext)
+
+        subs = soup.findAll('h3')
+        if subs:
+            subentries = []
+            for i, sub in enumerate(subs):
+                subentries.append({
+                    'title': sub.text,
+                    'anchor': u'#section%d.%d' % (section.id, i+1)
+                })
+
+            entry['sub'] = subentries
+
+        menu.append(entry)
+
+    from pprint import pprint
+    pprint(menu)
+
+    return {
+        'menu': menu,
+    }
